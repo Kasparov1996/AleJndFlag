e5e45f1c6e6f3954cff28143c86bf6df19fa00c8
Fakhri Zulkifli <0xabadass@gmail.com>
2018-05-11 02:11:18 +0800

    finish

--- get_flag.cpp
+++ get_flag.cpp
@@ -13,93 +13,29 @@
 #include "util.hpp"
 #include "sighandler.hpp"
 
-
-const std::string url("http://192.168.1.5:8888/scoreboard.json");
-
 int main(int argc, char **argv)
 {
     signal(SIGSEGV, segfault_handler);
+    signal(SIGABRT, segfault_handler);
     signal(SIGINT, sigint_handler);
 
-    uid_t user_id = getuid();
+    Json::Value jsonData;
+    Json::Reader jsonReader;
+
+    uid_t uid = getuid();
+    std::string local_ip = util::getLocalIP();
+    std::string scoreboard_data = util::getScoreboardData();
+
     int service_score = 0;
 
     for (int i = 0; i < (sizeof(service)/sizeof(service[0])); i++)
     {
-        if (user_id == service[i].uid)
+        if (uid == service[i].uid)
             service_score = service[i].score;
     }
 
-    /*
-     * TODO: Push to the scoreboard
-     */
-
-
-    CURL* curl = curl_easy_init();
-
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-
-    /* NOTE: if not in long datatype, segfault */
-    long httpCode(0);
-    std::unique_ptr<std::string> httpData(new std::string());
-
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, util::callback);
-
-    // Hook up data container (will be passed as the last parameter to the
-    // callback handling function).  Can be any pointer type, since it will
-    // internally be passed as a void pointer.
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
-
-    curl_easy_perform(curl);
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
-    curl_easy_cleanup(curl);
-
-    if (httpCode == 200)
-    {
-        std::cout << "\nGot successful response from " << url << std::endl;
-
-        Json::Value jsonData;
-        Json::Reader jsonReader;
-
-        if (jsonReader.parse(*httpData.get(), jsonData))
-        {
-            std::cout << "Successfully parsed JSON data" << std::endl;
-            std::cout << "\nJSON data received:" << std::endl;
-            std::cout << jsonData.toStyledString() << std::endl;
-
-            const std::string dateString(jsonData[0]["last_submit"].asString());
-            const std::size_t unixTimeMs(jsonData[0]["last_submit"].asUInt64());
-            const std::string timeString(jsonData[0]["last_submit"].asString());
-
-
-            // const std::string dateString(jsonData["date"].asString());
-            // const std::size_t unixTimeMs(
-            //         jsonData["milliseconds_since_epoch"].asUInt64());
-            // const std::string timeString(jsonData["time"].asString());
-            //
-            std::cout << "Natively parsed:" << std::endl;
-            std::cout << "\tDate string: " << dateString << std::endl;
-            std::cout << "\tUnix timeMs: " << unixTimeMs << std::endl;
-            std::cout << "\tTime string: " << timeString << std::endl;
-            std::cout << std::endl;
-        }
-        else
-        {
-            std::cout << "Could not parse HTTP data as JSON" << std::endl;
-            std::cout << "HTTP data was:\n" << *httpData.get() << std::endl;
-            std::cout << "Error: " << jsonReader.getFormattedErrorMessages() << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        std::cout << "Couldn't GET from " << url << " - exiting" << std::endl;
-        std::cout << "HTTP Code: " << httpCode << std::endl;
-        exit(EXIT_FAILURE);
-    }
+    std::string data = util::getServiceFlag(uid, local_ip, service_score);
+    std::cout << data << std::endl;
 
     exit(EXIT_SUCCESS);
 }
--- include/util.hpp
+++ include/util.hpp
@@ -16,6 +16,117 @@ namespace util
         return totalBytes;
     }
 
+    const std::string scoreboard_url("http://192.168.1.5:8888/scoreboard.json");
+
+    extern "C"
+    {
+        #include <sys/socket.h>
+        #include <arpa/inet.h>
+        #include <ifaddrs.h>
+
+        #include <stdio.h>
+        #include <stdlib.h>
+        #include <string.h>
+
+        char *getLocalIP();
+    };
+
+    char *getLocalIP()
+    {
+        struct ifaddrs *ifaces,  *res;
+        int ret;
+
+        if ((ret = getifaddrs(&ifaces)) != 0)
+        {
+            fprintf(stderr, "Failed to get interface.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        for (res = ifaces; res != NULL; res = res->ifa_next)
+        {
+            if (strncmp(res->ifa_name, "ens33", 5) !=0)
+                continue;
+
+            if (res->ifa_addr->sa_family == AF_INET)
+            {
+                freeifaddrs(ifaces);
+                return inet_ntoa(((struct sockaddr_in *)res->ifa_addr)->sin_addr);
+            }
+        }
+    }
+
+    std::string getServiceFlag(uid_t u, std::string local_ip, int s)
+    {
+        std::string uid = std::to_string(u);
+        std::string score = std::to_string(s);
+        const std::string flagsubmit_url("http://192.168.1.5:8888/flag/submit/" + uid + "/" + local_ip + "/" + score);
+
+        CURL* curl = curl_easy_init();
+        CURLcode res;
+
+        curl_easy_setopt(curl, CURLOPT_URL, flagsubmit_url.c_str());
+        curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        long httpCode(0);
+        std::unique_ptr<std::string> httpData(new std::string());
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, util::callback);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+
+        if ((res = curl_easy_perform(curl)) != CURLE_OK)
+        {
+            std::cerr << "Failed to send the request." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+        curl_easy_cleanup(curl);
+
+        if (httpCode == 200)
+            return *httpData.get();
+
+        return NULL;
+    }
+
+    std::string getScoreboardData()
+    {
+        CURL* curl = curl_easy_init();
+        CURLcode res;
+
+        curl_easy_setopt(curl, CURLOPT_URL, scoreboard_url.c_str());
+        curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        /* NOTE: if not in long datatype, segfault */
+        long httpCode(0);
+        std::unique_ptr<std::string> httpData(new std::string());
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, util::callback);
+
+        // Hook up data container (will be passed as the last parameter to the
+        // callback handling function).  Can be any pointer type, since it will
+        // internally be passed as a void pointer.
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+
+        if ((res = curl_easy_perform(curl)) != CURLE_OK)
+        {
+            std::cerr << "Failed to send a request." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+        curl_easy_cleanup(curl);
+
+        if (httpCode == 200)
+            return *httpData.get();
+
+        return NULL;
+    }
+
 
 }
 
