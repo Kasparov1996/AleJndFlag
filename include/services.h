bceb6397b0793bd4efb8696c978683de60973f6f
Fakhri Zulkifli <0xabadass@gmail.com>
2018-05-12 12:25:39 +0800

    no need for this

--- include/services.h
+++ include/services.h
@@ -3,18 +3,16 @@
 
 /*
  * uid - to match with the sqlite services uid
- * score - prevent conflicts with other services, and for pwn_deduct
  */
 struct services_info
 {
     int uid;
-    int score;
 };
 
 struct services_info service[] =
 {
-    {1000, 200},
-    {1001, 300}
+    {1000},
+    {1001}
 };
 
 #endif
